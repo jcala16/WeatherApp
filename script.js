7ff959560fe4d2385c66953f5d5474a15ed52323
Jake Cala <jcala36@stu.jjc.edu>
2025-04-16 13:41:23 -0500

    Added styling, and created capitalizeWords function

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// For my project, I'd like to build a weather app. The user will be able to enter any city into a search bar, and once they hit the search button they will see the current weather for the city,
+// as well as an upcoming forecast. Some icons will also appear. I will be using an api from openweathermap.org. I will build the project using the fetch() function in JavaScript, along with HTML and CSS. 
+// In the week 1 PR, I demonstrated the ability to recieve data from an API GET request, and display that data to the screen. For the Week 2 PR, I have created the prototype for the application. 
+// I had initially had created the bare bones functionality of the app, which displays the current weather data for a city. (no forecast) But the city had to be hardcoded in to API call, (line 16 in script.js). 
+// However, I spent some time each evening this week 'feature creeping' just for fun, and I've actually got the program pretty close to it's full functionality. I understand that it is now more
+// than a prototype, but I figured I should submit what I have regardless of the project being ahead of schedule. 
+// NOTE: the forecast city is still being hardcoded (line 17, currently set as 'Lockport'), and my next goal is to implement that feature via the search bar.
+// After that, I need to add some styling (maybe some bootstrap?) to make the application look visually pleasing. 
+// All of the code was written by me, following documentation from openweathermap.org.  
+
+// PART 3:
+// As of PR2, my project was mostly complete. I added the styling, and played around with the color pallete. I ended up staying with an Orange/Red theme, but I might change it later.
+// Additionally, I added a function that capitalizes the first letter of every word in the weather description, as the API return is all lowercase. I did this using a RegEx.
+//For PR4, im considering adding a "use current location" button.
+
+
 //This function executes when the button is pushed, and it grabs the weather info from the API
 function getWeather(){
     const apiKey = '60ff0493fc0209ccef573e52dc268352'; //my api key from the weather app https://openweathermap.org/
@@ -14,7 +30,7 @@ function getWeather(){
     //storing URLS
     
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Lockport&appid=${apiKey}&units=imperial`; 
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`; 
 
     //fetch current weather
     fetch(currentWeatherUrl)
@@ -39,6 +55,11 @@ function getWeather(){
         });
 }
 
+//This function will capitalize the first letter of every word in the description
+function capitalizeWords(str){
+    return str.replace(/\b\w/g, char => char.toUpperCase()); //Regex to capitalize first letter of each word
+}
+
 //This function displays the weather data to the HTML
 function displayWeather(data){
     //creating references to divs where info will be displayed
@@ -60,13 +81,12 @@ function displayWeather(data){
         //storing relevent data from API call in variable
         const cityName = data.name;
         const temperature = data.main.temp;
-        const description = data.weather[0].description;
+        const description = capitalizeWords(data.weather[0].description); //calling capitalizeWords function and storing output in const
         const iconCode = data.weather[0].icon;
         const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
 
         const temperatureHTML = `
             <p>${temperature}°F</p>`;
-        
         const weatherHTML = `
             <p>${cityName}</p>
             <p>${description}</p>`;
@@ -81,12 +101,13 @@ function displayWeather(data){
 
 }
 
+
 //This function displays the hourly forecast
 function displayHourlyForecast(hourlyData) {
     const hourlyForecastDiv = document.getElementById('hourly-forecast');
     //Debugging
     if (!hourlyForecastDiv) {
-        console.error("Element with ID 'hourly-forecast' not found.");
+        console.error("Element with ID 'hourly-forecast' not found."); //this led me to discover a typo in the div id in the html, I had named it "hourly-forcast", missing an 'e'
         return;
     }
 
